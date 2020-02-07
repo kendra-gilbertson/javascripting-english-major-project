993aa48ecb0fa38ab20d147c181ff22fd8966210
kendra-gilbertson <54083108+kendra-gilbertson@users.noreply.github.com>
2020-02-07 10:53:17 -0500

    commit

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
-let tipCalculator;
-tipCalculator = function(total, tipRate) {
-  // step 1:
-  let tipAmount;
-  tipAmount = tipRate * total;
-  // step 2:
-  $("#response").html("Your tip is $" + tipAmount);
+let myHabaneroSauceSquirts, myBurritoObject;
+myHabaneroSauceSquirts = 3;
+myBurritoObject = {
+  tortilla: "wheat",
+  guacamole: true,
+  beans: "pinto",
+  habaneroSauceSquirts: myHabaneroSauceSquirts,
+  spiciness: function(){
+    if (myHabaneroSauceSquirts > 0){
+      alert("This is a spicy burrito!");
+    } else {
+      alert ("This is a mild burrito!");
+    }
+  }
 };
-
-// Now call (or "execute") the function, passing a
-// total of $50.00 and a tipRate of 20%
-
-tipCalculator(50.00, .2);
+$("#response").html("Your burrito has " +
+myBurritoObject.habaneroSauceSquirts +
+" squirts of habanero.");
+myBurritoObject.spiciness();
